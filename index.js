aa449e7c2b1c5aafd2295b3c51a638d8759f1672
Sindre Sorhus <sindresorhus@gmail.com>
2018-03-22 22:35:22 +0700

    Remove moot `path.resolve` call
    `process.cwd()` is always absolute.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 const path = require('path');
 
-module.exports = input => path.resolve(input) === path.resolve(process.cwd());
+module.exports = input => path.resolve(input) === process.cwd();
